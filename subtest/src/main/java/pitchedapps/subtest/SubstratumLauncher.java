f8380f4263a61bd3869174f64538cc7c3ed38a64
AllanWang <me@allanwang.ca>
2016-12-24 04:35:34 -0500

    substrate update
    Former-commit-id: bf48184ace6659ba738b0a0a635243a072d69077 [formerly 534b1d8c3be0312bdc6134f3a058deeec1c70682]
    Former-commit-id: d62cae0c9eabbabc712ba56ac683eced1fe76dfe

---
+++ subtest/src/main/java/pitchedapps/subtest/SubstratumLauncher.java
@@ -0,0 +1,90 @@
+package pitchedapps.subtest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import projekt.substrate.LetsGetStarted;
+
+/**
+ * Created by Allan Wang on 2016-08-03.
+ */
+public class SubstratumLauncher extends Activity {
+
+    private static final String SUBSTRATUM_PACKAGE_NAME = "projekt.substratum";
+
+    private boolean isPackageInstalled(String package_name) {
+        PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo(package_name, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isPackageEnabled(String package_name) {
+        try {
+            ApplicationInfo ai = getPackageManager().getApplicationInfo(package_name, 0);
+            return ai.enabled;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void beginSubstratumLaunch() {
+        // If Substratum is found, then launch it with specific parameters
+        if (isPackageInstalled(SUBSTRATUM_PACKAGE_NAME)) {
+            if (!isPackageEnabled(SUBSTRATUM_PACKAGE_NAME)) {
+                Toast toast = Toast.makeText(this, getString(R.string.toast_substratum_frozen),
+                        Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+            // Substratum is found, launch it directly
+            launchSubstratum();
+        } else {
+            getSubstratumFromPlayStore();
+        }
+    }
+
+    private void getSubstratumFromPlayStore() {
+        String playURL =
+                "https://play.google.com/store/apps/details?" +
+                        "id=projekt.substratum&hl=en";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        Toast toast = Toast.makeText(this, getString(R.string.toast_substratum),
+                Toast.LENGTH_SHORT);
+        toast.show();
+        i.setData(Uri.parse(playURL));
+        startActivity(i);
+        finish();
+    }
+
+    private void launchSubstratum() {
+        Intent intent = LetsGetStarted.begin(getApplicationContext(),
+                getIntent(), getString(R.string.ThemeName), getPackageName(),
+                getString(R.string.unauthorized), false);
+        if (intent != null) {
+            startActivity(intent);
+        }
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        launch();
+    }
+
+    private void launch() {
+        beginSubstratumLaunch();
+    }
+
+
+}
