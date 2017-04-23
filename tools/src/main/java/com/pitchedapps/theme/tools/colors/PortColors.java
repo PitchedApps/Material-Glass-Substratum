34886b3ff23404faf98dc8d136526a1dd8cb3e74
Allan Wang <me@allanwang.ca>
2017-04-22 23:29:17 -0400

    more tests and backup [skip ci]

--- tools/src/main/java/com/pitchedapps/theme/tools/colors/PortColors.java
+++ tools/src/main/java/com/pitchedapps/theme/tools/colors/PortColors.java
@@ -1,29 +1,33 @@
-package com.pitchedapps.theme.tools;
+package com.pitchedapps.theme.tools.colors;
 
+import com.pitchedapps.theme.tools.Config;
 import com.pitchedapps.theme.tools.utils.ColorData;
 import com.pitchedapps.theme.tools.utils.FileUtils;
 import com.pitchedapps.theme.tools.utils.Utils;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class PortColors {
-    static String packageName = "com.android.vending",
+    public static String packageName = "com.android.vending",
             solution = "Google",
             project = "Google Play Store_7.6.08";
 
-    static String writeDir = Config.defaultWrite + "values", //values directory to write new color file
+    public static String writeDir = Config.defaultWrite + "values", //values directory to write new color file
             baseDir = Config.getVTSDir(solution, project), //values source directory
             themeDir = Config.getThemedDir(packageName); //values themed directory
-    static final String COLORS_XML = "colors.xml";
+
+    public static final String COLORS_XML = "colors.xml",
+            DEFAULT_VALUES = "values-v21";
+
+    public static void backup() {
+        FileUtils.copyFile(themeDir + DEFAULT_VALUES, COLORS_XML, writeDir, "colors.backup.xml");
+    }
 
     public static void main(String[] args) {
-
+        backup();
         //initialize variables
         Map<String, ColorData> refs = new HashMap<>(); //holds all name data relations
         Set<String> sourceNames = new LinkedHashSet<>(); //holds all the color names in the source
@@ -34,6 +38,7 @@ public class PortColors {
         Set<ColorData> stillThemed = new LinkedHashSet<>(); //holds colors in themed that are in source
 
         //Add titles
+        stillThemed.add(new ColorData().verbatim(Utils.title(packageName)));
         missing.add(new ColorData().verbatim(Utils.title("missing")));
         removed.add(new ColorData().verbatim(Utils.title("removed")));
 
@@ -62,7 +67,7 @@ public class PortColors {
         sourceNames.forEach(s -> missing.add(refs.get(s)));
 
         //Create file
-        FileUtils.writeFile(writeDir, "colors.xml", stillThemed, removed, missing);
+        FileUtils.writeFile(writeDir, COLORS_XML, stillThemed, removed, missing);
 
     }
 
---
+++ tools/src/main/java/com/pitchedapps/theme/tools/colors/ProduceColors.java
@@ -0,0 +1,26 @@
+package com.pitchedapps.theme.tools.colors;
+
+import com.pitchedapps.theme.tools.utils.FileUtils;
+import com.pitchedapps.theme.tools.utils.Utils;
+
+/** 
+ * Created by Allan Wang on 2017-04-22.
+ * <p>
+ * When ready, creates production ready color.xml
+ */
+
+public class ProduceColors {
+
+    public static void main(String[] args) {
+        StringBuilder content = FileUtils.readFile(PortColors.writeDir, PortColors.COLORS_XML);
+        if (content == null) {
+            Utils.p("No color.xml ported yet");
+            System.exit(0);
+        }
+        FileUtils.writeFile(FileUtils.filename(PortColors.themeDir, PortColors.DEFAULT_VALUES),
+                PortColors.COLORS_XML, content.toString()
+                        .replaceAll("(<!--#.*-->)", "") //remove pseudocomments
+                        .replaceAll("(\n[\n\t ]+)", "\n\t")); //remove multiple newLines
+    }
+
+}
