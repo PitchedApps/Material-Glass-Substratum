416e36186e27611faddd3b358dfbb41bab9d3b4c
Allan Wang <me@allanwang.ca>
2017-04-24 04:00:30 -0400

    fail on no log
    Former-commit-id: f0896596dbb0c666b556f1c1db478619f2a63e00 [formerly 24b6109ef50bfe39a2efaf757be40451b3c37bc7]
    Former-commit-id: f96b10ac30aa7184b842c531a294ab382b3141f4

--- overlay-verify.sh
+++ overlay-verify.sh
@@ -3,15 +3,18 @@
 # Compile our overlays against an android framework to ensure there are no compile errors
 
 echo "Cloning Builder"
-git clone --branch=master  https://github.com/PitchedApps/Substratum-Builder-Resources.git
+git clone --quiet --branch=master  https://github.com/PitchedApps/Substratum-Builder-Resources.git
 
 overlays=$PWD/substratum/src/main/assets/overlays
 printf "Testing overlays at %s\n" "$overlays"
 
 cd Substratum-Builder-Resources
 
-echo `ls -l`
 sh build-overlays.sh "$overlays"    # run builder
+
+if [ ! -f "builds/log.txt" ]; then  # overlays didn't build properly
+    exit 1
+fi
 
 if [ -s "builds/log.txt" ]; then    # error occurred
     sh ../generate-apk-release.sh   # this will release the error
