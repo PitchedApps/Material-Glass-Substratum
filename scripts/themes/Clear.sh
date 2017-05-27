cd5ce54c60ccacc9aeee90f747eaf6ae403d0ae6
Allan Wang <me@allanwang.ca>
2017-05-27 10:27:59 -0700

    red and test duplicate
    Former-commit-id: 6644a452f5e21ab3c0306dcfa558cd9d2675a711 [formerly 52659d002512af782eebe547e1574ac88174af0f]
    Former-commit-id: e833fb37c67f02b420da5306b5454dd18bdc1c85

--- scripts/themes/Clear.sh
+++ scripts/themes/Clear.sh
@@ -2,7 +2,6 @@
 
 themeXml() {
     echo "$(sed \
-    -e 's/\$\$o\$/@android:drawable\/screen_background_dark/gi' \
     -e 's/\$\$b\$/@android:drawable\/screen_background_dark_transparent/gi' \
     -e 's/\$t\$\([a-z0-9]\{2\}\)/#\1ffffff/gi' \
     -e 's/\$t\$/#fff/gi' \
@@ -12,8 +11,10 @@ themeXml() {
     -e 's/\$b\$/#80000000/gi' \
     -e 's/\$bb\$/#80111111/gi' \
     -e 's/\$bbb\$/#80222222/gi' \
+    -e 's/\$bt\$/#a0000000/gi' \
+    -e 's/\$btt\$/#a0111111/gi' \
+    -e 's/\$bttt\$/#a0222222/gi' \
     -e 's/\$c\$/#30000000/gi' \
-    -e 's/\$d\$/#20ffffff/gi' \
     -e 's/\$o\$/#000/gi' \
     -e 's/\$oo\$/#111/gi' \
     -e 's/\$ooo\$/#222/gi' \
@@ -24,6 +25,7 @@ themeXml() {
     -e 's/\$rr\$/#10ffffff/gi' \
     -e 's/\$d\$/#d0060606/gi' \
     -e 's/\$dd\$/#a0000000/gi' \
+    -e 's/\$ddd\$/#a0111111/gi' \
     -e 's/\$m\$/#888/gi' \
     -e 's/\$mm\$/#50888888/gi' \
     -e 's/\$wall\$/true/gi' \
--- scripts/themes/Dark.sh
+++ scripts/themes/Dark.sh
@@ -2,7 +2,6 @@
 
 themeXml() {
     echo "$(sed \
-    -e 's/\$\$o\$/@android:drawable\/screen_background_dark/gi' \
     -e 's/\$\$b\$/@android:drawable\/screen_background_dark/gi' \
     -e 's/\$t\$\([a-z0-9]\{2\}\)/#\1ffffff/gi' \
     -e 's/\$t\$/#fff/gi' \
@@ -12,8 +11,10 @@ themeXml() {
     -e 's/\$b\$/#000/gi' \
     -e 's/\$bb\$/#111/gi' \
     -e 's/\$bbb\$/#222/gi' \
+    -e 's/\$bt\$/#a0000000/gi' \
+    -e 's/\$btt\$/#a0111111/gi' \
+    -e 's/\$bttt\$/#a0222222/gi' \
     -e 's/\$c\$/#111/gi' \
-    -e 's/\$d\$/#20ffffff/gi' \
     -e 's/\$o\$/#000/gi' \
     -e 's/\$oo\$/#111/gi' \
     -e 's/\$ooo\$/#222/gi' \
@@ -24,6 +25,7 @@ themeXml() {
     -e 's/\$rr\$/#10ffffff/gi' \
     -e 's/\$d\$/#060606/gi' \
     -e 's/\$dd\$/#000/gi' \
+    -e 's/\$ddd\$/#111/gi' \
     -e 's/\$m\$/#888/gi' \
     -e 's/\$mm\$/#50888888/gi' \
     -e 's/\$wall\$/false/gi' \
